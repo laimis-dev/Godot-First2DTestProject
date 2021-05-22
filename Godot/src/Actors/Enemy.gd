4d9a2cc780c6b4ebb6b9aa07cebb79739ac7851e
Laimonas Janutėnas <laimonas.janutenas@gmail.com>
2021-05-22 10:13:19 +0300

    finished first part

--- Godot/src/Actors/Enemy.gd
+++ Godot/src/Actors/Enemy.gd
@@ -1,10 +1,19 @@
 extends "res://src/Actors/Actor.gd"
 
 func _ready():
+	set_physics_process(false)
 	velocity.x = -speed.x
 
 func _physics_process(delta):
+	velocity.y += gravity * delta
 	if is_on_wall():
 		velocity.x *= -1.0
 		
-	velocity = move_and_slide(velocity, FLOOR_NORMAL)
+	velocity.y = move_and_slide(velocity, FLOOR_NORMAL).y
+
+
+func _on_stompDetector_body_entered(body):
+	#if body.global_position.y < get_node("stompDetector").global_position.y:
+		#return
+	get_node("CollisionShape2D").disabled = true
+	queue_free()
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Godot-FirstTestProject
  
-Source: https://www.youtube.com/watch?v=Mc13Z2gboEk&t=175s
+Source: https://www.youtube.com/watch?v=Mc13Z2gboEk
